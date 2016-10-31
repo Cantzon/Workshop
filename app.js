31a6e204797fee76ae48058a089ad6e863ad11c2
Hari Krishna <Hari Krishna>
2016-10-31 13:05:55 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+.card{
+	height: 189px;
+	width: 300px;
+	background-color: red;
+	border-radius: 5px;
+	box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.50)
+}
+
+.jumbo-number{
+	font-siz: 80px
+	font-family: arial;
+}
+
+.fact-text{
+	color: white;
+	font-siz: 20px;
+	font-weight: 200;
+}
